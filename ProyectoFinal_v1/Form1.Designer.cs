a9b080db9b3220fb6117728896efe3b2bea09b2a
DevWolf86 <romandev86@gmail.com>
2022-06-02 15:10:57 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinal_v1/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ProyectoFinal_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.GrdUsuarios = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.TxtApellido = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CmRol = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GrdUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 132);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(504, 354);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.GrdUsuarios);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(496, 328);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Usuarios";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // GrdUsuarios
+            // 
+            this.GrdUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GrdUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Apellido,
+            this.Rol});
+            this.GrdUsuarios.Location = new System.Drawing.Point(9, 17);
+            this.GrdUsuarios.Name = "GrdUsuarios";
+            this.GrdUsuarios.Size = new System.Drawing.Size(745, 291);
+            this.GrdUsuarios.TabIndex = 0;
+            this.GrdUsuarios.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GrdUsuarios_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(25, 46);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(146, 20);
+            this.txtname.TabIndex = 2;
+            // 
+            // TxtApellido
+            // 
+            this.TxtApellido.Location = new System.Drawing.Point(206, 45);
+            this.TxtApellido.Name = "TxtApellido";
+            this.TxtApellido.Size = new System.Drawing.Size(134, 20);
+            this.TxtApellido.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Apellido";
+            // 
+            // CmRol
+            // 
+            this.CmRol.FormattingEnabled = true;
+            this.CmRol.Location = new System.Drawing.Point(367, 43);
+            this.CmRol.Name = "CmRol";
+            this.CmRol.Size = new System.Drawing.Size(121, 21);
+            this.CmRol.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(367, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rol";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(136, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            // 
+            // Rol
+            // 
+            this.Rol.HeaderText = "Rol";
+            this.Rol.Name = "Rol";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 512);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CmRol);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtApellido);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.GrdUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView GrdUsuarios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox TxtApellido;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox CmRol;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rol;
+    }
+}
+
---
+++ ProyectoFinal_v1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OracleClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal_v1
+{
+    public partial class Form1 : Form
+    {
+        private int n = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Adicionar usuarios a tabla
+            int n = GrdUsuarios.Rows.Add();
+
+            // diligenciamos los campos
+            GrdUsuarios.Rows[n].Cells[0].Value = txtname.Text;
+            GrdUsuarios.Rows[n].Cells[1].Value = TxtApellido.Text;
+            GrdUsuarios.Rows[n].Cells[2].Value = CmRol.Text;
+
+            // Se limpian los campos
+            txtname.Text = "";
+            TxtApellido.Text = "";
+            CmRol.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CmRol.Items.Add("Administrador");
+            CmRol.Items.Add("Supervisor");
+            CmRol.Items.Add("Coordinador");
+            CmRol.Items.Add("Tecnico");
+            CmRol.Items.Add("Soporte");
+            CmRol.Items.Add("Usuario");
+
+            // Conexion a oracle
+
+            //OracleConnection ora = new OracleConnection();
+            //ora.Open();
+            //ora.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Elimina la celda cargada
+            if (n != -1)
+            {
+                GrdUsuarios.Rows.RemoveAt(n);
+            }
+        }
+
+        private void GrdUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Selecciona la celda para poder elimianrla 
+            n = e.RowIndex;
+
+            if (n != -1)
+            {
+                txtname.Text = (string)GrdUsuarios.Rows[n].Cells[0].Value;
+                TxtApellido.Text = (string)GrdUsuarios.Rows[n].Cells[1].Value;
+            }
+        }
+    }
+}
